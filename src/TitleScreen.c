868d6460d891bc2c8c990777d300bda6dc256408
hdfsyu <themultiverse495@gmail.com>
2023-11-11 16:03:12 -0500

    Initial commit

---
+++ src/TitleScreen.c
@@ -0,0 +1,8 @@
+#include <raylib.h>
+// Loads the title screen of the game.
+void titleScreen(int screenWidth, int screenHeight){
+    BeginDrawing();
+    ClearBackground(GRAY);
+    DrawRectangle(screenWidth / 2, 0, 128, 64, BLACK);
+    EndDrawing();
+}
